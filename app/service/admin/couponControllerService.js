aeddb963d9be57b96578b65abc8d362f5f2337f7
Looperzero007 <prasantht861@gmail.com>
2026-01-16 10:20:04 +0530

    fix: improve the coupon validatin

--- app/service/admin/couponControllerService.js
+++ app/service/admin/couponControllerService.js
@@ -38,6 +38,18 @@ export const createNewCoupon = async (couponData) => {
 };
 
 export const updateCouponStatus = async (couponId, status) => {
+  const couponData = await Coupon.findById('couponId')
+
+  const {code} = couponData
+
+  const existingCoupon = await Coupon.findOne({
+    code: { $regex: new RegExp(`^${code}$`, "i") },
+  });
+
+  if (existingCoupon) {
+    throw new Error("A coupon with this code already exists.");
+  }
+
   const coupon = await Coupon.findByIdAndUpdate(
     couponId,
     { status: status },
